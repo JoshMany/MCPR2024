c2f312e4c81c513e1e65b40a91938bd3052c14b6
Jose Manuel <jose.munoz@techgenies.com>
2023-09-09 10:20:44 -0600

    Desktop fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,12 @@ import image4 from "/photos/img4.jpg";
 import logo1 from "/logos/IAPR.png";
 import logo2 from "/logos/INAOE.png";
 import logo3 from "/logos/LNCS.jpeg";
-import logo4 from "/logos/UV.svg";
+import logo4 from "/logos/UV.png";
 import { Autoplay } from "swiper/modules";
+import "./Swiper.css";
 function App() {
+  const photos = [image1, image2, image3, image4];
+
   const InfiniteSlider = () => {
     return (
       <div className="relative flex h-min flex-col justify-center overflow-hidden bg-transparent py-6 sm:py-12">
@@ -69,20 +72,24 @@ function App() {
 
   return (
     <>
-      <div className="bg-white h-screen w-screen">
+      <div className="bg-white">
         <Header />
-        <div className="relative h-[400px] w-auto mx-6 ">
+
+        <div className="relative h-fit mx-6 max-w-screen min-h-[400px] max-h-[600px]">
           <div className="absolute flex flex-col rounded-3xl h-full w-full z-10 p-4 justify-between">
-            <p className="text-5xl text-white font-bold w-1/3">
-              Mexican Conference on Pattern Recognition
-            </p>
             <div>
-              <p className="text-xl font-bold text-white ">June 21-24, 2024</p>
+              <p className="text-5xl text-white font-bold w-full max-w-lg">
+                Mexican Conference on Pattern Recognition
+              </p>
+
+              <p className="text-xl font-bold text-white mt-4">
+                June 21-24, 2024
+              </p>
               <p className="text-lg font-bold text-white ">
                 Xalapa, Veracruz, México
               </p>
             </div>
-            <button className="bg-cyan-500 text-white text-base font-medium px-5 py-2 rounded-full self-center">
+            <button className="bg-cyan-500 text-white text-base font-medium px-5 py-2 rounded-full self-center md:self-start">
               Get the eBook now
             </button>
           </div>
@@ -92,222 +99,189 @@ function App() {
             loop={true}
             centeredSlides={true}
             autoplay={{
-              delay: 5000,
+              delay: 3000,
               disableOnInteraction: false,
             }}
             modules={[Autoplay]}
-            className="rounded-3xl h-full w-auto"
+            className="rounded-3xl aspect-square min-h-[400px] max-h-[500px]"
           >
-            <SwiperSlide>
-              <img
-                src={image1}
-                alt=""
-                className="object-cover object-center h-full z-0 brightness-75"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={image2}
-                alt=""
-                className="object-cover object-center h-full brightness-75"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={image3}
-                alt=""
-                className="object-cover object-center h-full brightness-75"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={image4}
-                alt=""
-                className="object-cover object-center h-full brightness-75"
-              />
-            </SwiperSlide>
+            {photos.map((photo, id) => (
+              <SwiperSlide key={id} className="h-min-[450px] h-max-[500px]">
+                <div className="h-full">
+                  <img
+                    src={photo}
+                    alt=""
+                    className="brightness-75 h-fit w-auto object-cover object-center"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
         <InfiniteSlider />
-        <div className="rounded-3xl w-auto p-4 mx-6 bg-[#b7bffe] my-2 flex flex-col">
-          <h2 className="text-2xl font-bold text-[#111321]">
-            About the congress
-          </h2>
-          <p className="text-lg my-4 text-[#111321]">
-            The 16th Mexican Conference on Pattern Recognition (MCPR2024) will
-            be held in Xalapa, Veracruz, Mexico, from June 20 to June 23, 2024,
-            under the direction of the Xalapa Technological Transfer Unit of the
-            Center for Scientific and Higher Education Research of Ensenada,
-            Baja California (CICESE-UT3), in collaboration with the Veracruzano
-            Center for Innovation and Technology Transfer and the Coordination
-            of Computational Sciences of the National Institute of Astrophysics,
-            Optics and Electronics (INAOE), and with the consent of the
-            International Association for Pattern Recognition (IAPR) and the
-            Mexican Association for Computer Vision, Neurocomputing and Robotics
-            (MACVNR), which is affiliated to the IAPR. MCPR2024 aims to provide
-            a forum for the exchange of scientific results, experiences, and new
-            knowledge, as well as promoting cooperation among research groups in
-            Pattern Recognition and related areas in Mexico and around the
-            world.
-          </p>
-          <button className="self-center w-3/4 px-6 py-3 text-lg bg-[#111321] text-white rounded-full">
-            Previuos Editions
-          </button>
+        <div className="flex flex-col lg:flex-row">
+          <div className="rounded-3xl w-auto p-4 mx-6 bg-[#b7bffe] my-2 flex flex-col lg:mt-2 lg:my-0 lg:mb-0 lg:w-1/2">
+            <h2 className="text-2xl font-bold text-[#111321]">
+              About the congress
+            </h2>
+            <p className="text-lg my-4 text-[#111321]">
+              The 16th Mexican Conference on Pattern Recognition (MCPR2024) will
+              be held in Xalapa, Veracruz, Mexico, from June 20 to June 23,
+              2024, under the direction of the Xalapa Technological Transfer
+              Unit of the Center for Scientific and Higher Education Research of
+              Ensenada, Baja California (CICESE-UT3), in collaboration with the
+              Veracruzano Center for Innovation and Technology Transfer and the
+              Coordination of Computational Sciences of the National Institute
+              of Astrophysics, Optics and Electronics (INAOE), and with the
+              consent of the International Association for Pattern Recognition
+              (IAPR) and the Mexican Association for Computer Vision,
+              Neurocomputing and Robotics (MACVNR), which is affiliated to the
+              IAPR. MCPR2024 aims to provide a forum for the exchange of
+              scientific results, experiences, and new knowledge, as well as
+              promoting cooperation among research groups in Pattern Recognition
+              and related areas in Mexico and around the world.
+            </p>
+            <button className="self-center w-3/4 px-6 py-3 text-lg bg-[#111321] text-white rounded-full">
+              Previuos Editions
+            </button>
+          </div>
+          <div className="rounded-3xl w-auto p-4 mx-6 bg-[#84c6d2] mt-4 mb-8 flex flex-col lg:mt-2 lg:my-0 lg:mb-0 lg:w-1/2">
+            <h2 className="text-2xl font-bold text-[#04050d]">
+              Important dates
+            </h2>
+            <img src="" alt="" />
+            <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-4 ml-4">
+              <li className="mb-10 ml-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-md font-normal leading-none text-[#222222]">
+                  February 20, 2024
+                </time>
+                <h3 className="text-lg font-semibold text-[#04050d]">
+                  Submission deadline
+                </h3>
+              </li>
+              <li className="mb-10 ml-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-md font-normal leading-none text-[#222222]">
+                  March 27, 2024
+                </time>
+                <h3 className="text-lg font-semibold text-[#04050d]">
+                  Notification
+                </h3>
+              </li>
+              <li className="ml-4 mb-10">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-md font-normal leading-none text-[#222222]">
+                  April 3, 2024
+                </time>
+                <h3 className="text-lg font-semibold text-[#04050d]">
+                  Camera-ready due
+                </h3>
+              </li>
+              <li className="mb-10 ml-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-md font-normal leading-none text-[#222222]">
+                  April 22, 2024
+                </time>
+                <h3 className="text-lg font-semibold text-[#04050d]">
+                  Early bird registration
+                </h3>
+              </li>
+              <li className="ml-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-md font-normal leading-none text-[#222222]">
+                  June 21-24, 2024
+                </time>
+                <h3 className="text-lg font-semibold text-[#04050d]">
+                  Main conference
+                </h3>
+              </li>
+            </ol>
+          </div>
         </div>
-        <div className="rounded-3xl w-auto p-4 mx-6 bg-[#84c6d2] mt-4 mb-8 flex flex-col">
-          <h2 className="text-2xl font-bold text-[#04050d]">Important dates</h2>
-          <img src="" alt="" />
-          <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-4 ml-4">
-            <li className="mb-10 ml-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-[#222222]">
-                February 20, 2024
-              </time>
-              <h3 className="text-lg font-semibold text-[#04050d]">
-                Submission deadline
-              </h3>
-            </li>
-            <li className="mb-10 ml-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-[#222222]">
-                March 27, 2024
-              </time>
-              <h3 className="text-lg font-semibold text-[#04050d]">
-                Notification
-              </h3>
-            </li>
-            <li className="ml-4 mb-10">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-[#222222]">
-                April 3, 2024
-              </time>
-              <h3 className="text-lg font-semibold text-[#04050d]">
-                Camera-ready due
-              </h3>
-            </li>
-            <li className="mb-10 ml-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-[#222222]">
-                April 22, 2024
-              </time>
-              <h3 className="text-lg font-semibold text-[#04050d]">
-                Early bird registration
-              </h3>
-            </li>
-            <li className="ml-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-[#222222]">
-                June 21-24, 2024
-              </time>
-              <h3 className="text-lg font-semibold text-[#04050d]">
-                Main conference
-              </h3>
-            </li>
-          </ol>
-        </div>
-
         <footer className="bg-white dark:bg-gray-900">
-          <div className="mx-auto w-full max-w-screen-xl">
+          <div className="mx-auto w-screen max-w-full">
             <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-              <div>
+              <div className="w-full">
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Company
+                  Links
                 </h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
                     <a href="#" className=" hover:underline">
-                      About
+                      Home
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Careers
+                      Call for papers
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Brand Center
+                      Paper Awards
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Blog
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Help center
-                </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Discord Server
+                      Keynote Speakers
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Twitter
+                      Student´s Meeting
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Facebook
+                      Info and Tours
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Contact Us
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Legal
-                </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Privacy Policy
+                      Program Committee
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Licensing
+                      Organization
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Terms &amp; Conditions
+                      Submission Guidelines
+                    </a>
+                  </li>
+                  <li className="mb-4">
+                    <a href="#" className="hover:underline">
+                      Fees for Authors
                     </a>
                   </li>
                 </ul>
               </div>
               <div>
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Download
+                  Contact
                 </h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      iOS
+                      Ansel Y. Rodríguez González
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Android
+                      Congress organizer
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      Windows
+                      ansel@cicese.edu.mx
                     </a>
                   </li>
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
-                      MacOS
+                      mcpr2023@inaoep.mx{" "}
                     </a>
                   </li>
                 </ul>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,97 +1,157 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { Bars4Icon, ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Menu, Transition } from "@headlessui/react";
+import { Dropdown } from "flowbite-react";
 
 export default function Header() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+  const BrandNameAnchor = () => {
+    return (
+      <h1 className="text-pink-600 uppercase text-3xl font-bold">
+        <a href={"/"}>MCPR2024</a>
+      </h1>
+    );
+  };
 
-  const DropdownButtonSM = ({
+  const MenuBtn = () => {
+    return (
+      <button
+        className="lg:hidden"
+        onClick={() => {
+          setDrawerState(!drawerState);
+        }}
+      >
+        <Bars4Icon className="text-pink-600 w-8 h-8" />
+      </button>
+    );
+  };
+
+  const NavElement = ({
     label,
     items,
+    style,
   }: {
     label: string;
-    items: string[];
-  }) => (
-    <Menu>
-      <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 items-center py-2 text-xl font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-        {label}
-        <ChevronDownIcon
-          className="ml-2 -mr-1 h-5 w-5 text-white hover:text-white"
-          aria-hidden="true"
-        />
-      </Menu.Button>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+    items: [{ label: string; path: string }];
+    style?: string;
+  }) => {
+    return (
+      <Dropdown
+        label
+        placement="bottom"
+        inline
+        size={"lg"}
+        className={` -ml-4 ${style == "nav" ? "bg-white" : "bg-black"}`}
+        renderTrigger={() => (
+          <button
+            className={`${
+              style == "nav" ? "text-black text-xl" : "text-white"
+            }  flex flex-row w-fit items-center`}
+          >
+            {label}
+            <ChevronDownIcon
+              className={`${style == "nav" ? "h-5 w-5" : "h-7 w-7"}`}
+            />
+          </button>
+        )}
       >
-        <Menu.Items className="relative mt-2 w-1/2 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1 ">
-            {items.map((item) => (
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? "bg-orange-500 text-white" : "text-gray-900"
-                    } group flex justify-center w-full items-center rounded-md px-2 py-2 text-lg`}
-                  >
-                    {item}
-                  </button>
-                )}
-              </Menu.Item>
-            ))}
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-  );
+        {items.map((item, id) => (
+          <Dropdown.Item
+            key={id}
+            as={"a"}
+            href={item.path}
+            className={`${
+              style == "nav" ? "text-black" : "text-white"
+            } text-lg  w-full px-6 py-1 bg-transparent`}
+          >
+            {item.label}
+          </Dropdown.Item>
+        ))}
+      </Dropdown>
+    );
+  };
 
-  const DisplayableMenu = () => (
-    <div
-      className={`absolute z-50 h-screen w-screen top-0 -left-full bg-black  ${
-        isMenuOpen && "-left-0"
-      }`}
-    >
-      <div className="w-full flex flex-row items-center justify-between p-6">
-        <p className="text-3xl font-bold text-white">Menu</p>
-        <button onClick={() => setMenuOpen(!isMenuOpen)}>
-          <Bars4Icon className="h-10 w-10 text-white" />
-        </button>
-      </div>
+  const [drawerState, setDrawerState] = useState(false);
+  return (
+    <div className="flex flex-row justify-between px-6 w-full my-3">
+      <BrandNameAnchor />
+      <MenuBtn />
+      <nav
+        className={`top-0 absolute transition-all flex flex-col bg-black h-screen w-full px-6 z-50 overscroll-none ${
+          drawerState ? "-left-0 " : "-left-[200%]"
+        }`}
+      >
+        <div className="flex flex-row justify-between w-full as-center my-3">
+          <BrandNameAnchor />
+          <MenuBtn />
+        </div>
+        <ul className="relative flex flex-col mt-6 text-2xl gap-5 items-end">
+          <NavElement label="Main" items={[{ label: "Home", path: "/" }]} />
+          <NavElement
+            label="Contributions"
+            items={[{ label: "Submission Guidelines", path: "/" }]}
+          />
+          <NavElement
+            label="Attendence"
+            items={[{ label: "Fees for author", path: "/" }]}
+          />
 
-      <ul className="w-full flex flex-col justify-center">
-        <li>
-          <DropdownButtonSM
-            label="Main"
+          <NavElement
+            label="Previous MCPRs"
             items={[
-              "Home",
-              "Call for papers",
-              "Paper Awards",
-              "Keynote Speakers",
-              "Student's Meeting",
+              { label: "MCPR 2022", path: "/" },
+              { label: "MCPR 2021", path: "/" },
+              { label: "MCPR 2020", path: "/" },
+              { label: "MCPR 2019", path: "/" },
+              { label: "MCPR 2018", path: "/" },
+              { label: "MCPR 2017", path: "/" },
+              { label: "MCPR 2016", path: "/" },
+              { label: "MCPR 2015", path: "/" },
+              { label: "MCPR 2014", path: "/" },
+              { label: "MCPR 2013", path: "/" },
+              { label: "MCPR 2012", path: "/" },
+              { label: "MCPR 2011", path: "/" },
+              { label: "MCPR 2010", path: "/" },
             ]}
           />
-        </li>
-        <li>
-          <DropdownButtonSM label="Contributions" items={["Z", "X", "Y"]} />
-        </li>
-      </ul>
-    </div>
-  );
-
-  return (
-    <>
-      <div className="w-screen flex flex-row justify-between p-6 ">
-        <h1 className="text-3xl font-bold text-pink-600">MCPR2023</h1>
-        <button onClick={() => setMenuOpen(!isMenuOpen)}>
-          <Bars4Icon className="h-9 w-9 text-pink-600" />
-        </button>
-        {isMenuOpen && <DisplayableMenu />}
+        </ul>
+      </nav>
+      <div className="hidden lg:flex">
+        <ul className="flex flex-row text-3xl gap-10">
+          <NavElement
+            style="nav"
+            label="Main"
+            items={[{ label: "Home", path: "/" }]}
+          />
+          <NavElement
+            label="Contributions"
+            style="nav"
+            items={[{ label: "Submission Guidelines", path: "/" }]}
+          />
+          <NavElement
+            label="Attendence"
+            style="nav"
+            items={[{ label: "Fees for author", path: "/" }]}
+          />
+          <NavElement
+            label="Previous MCPRs"
+            style="nav"
+            items={[
+              { label: "MCPR 2022", path: "/" },
+              { label: "MCPR 2021", path: "/" },
+              { label: "MCPR 2020", path: "/" },
+              { label: "MCPR 2019", path: "/" },
+              { label: "MCPR 2018", path: "/" },
+              { label: "MCPR 2017", path: "/" },
+              { label: "MCPR 2016", path: "/" },
+              { label: "MCPR 2015", path: "/" },
+              { label: "MCPR 2014", path: "/" },
+              { label: "MCPR 2013", path: "/" },
+              { label: "MCPR 2012", path: "/" },
+              { label: "MCPR 2011", path: "/" },
+              { label: "MCPR 2010", path: "/" },
+            ]}
+          />
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
